48fdc66503513bfb8303970ee4b095519ab617c0
cristianhoyos66 <cristianhoyos66@hotmail.com>
2017-05-14 19:50:13 -0500

    Doing a concept test with the angular2-notifications library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NotificationsService } from 'angular2-notifications';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+    constructor(private _service: NotificationsService) {}
+
+    public options = {
+        position: ["top", "right"],
+        lastOnBottom: true,
+        timeOut: 999 
+    };
+
+    create() {
+        this._service.error(
+            'Some Title',
+            'Some content',
+            {
+                showProgressBar: true,
+                pauseOnHover: true,
+                clickToClose: true
+            }
+        )
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { SimpleNotificationsModule } from 'angular2-notifications';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { NotificationsComponent } from './notifications/notifications.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotificationsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    SimpleNotificationsModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
